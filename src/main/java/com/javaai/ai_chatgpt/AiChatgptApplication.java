3bbc994e5a301e5fd4f5813966a31caf91b4ae3c
Spaesh Mavi <sparshmavi8@gmail.com>
2024-11-10 19:09:26 +0530

    files added

---
+++ src/main/java/com/javaai/ai_chatgpt/AiChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.javaai.ai_chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatgptApplication.class, args);
+	}
+
+}
